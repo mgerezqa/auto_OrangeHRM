fcf0d1c40ff1fea5aacf1ec466898661f949be74
Melli11 <mingerez@gmail.com>
2023-04-25 20:27:48 -0300

    Inicio de Sesión OK

---
+++ src/test/java/paginas/PaginaInicio.java
@@ -0,0 +1,33 @@
+package paginas;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaginaInicio {
+
+	@FindBy(name ="username")
+	WebElement txtName;
+			
+	@FindBy(name ="password")
+	WebElement txtPassword;
+
+	@FindBy(tagName = "button")
+	WebElement buttonLogin;
+
+	public PaginaInicio(WebDriver driver){
+	PageFactory.initElements(driver, this);
+	}
+	
+	public void ingresarCredenciales(String user, String password) {
+		txtName.sendKeys(user);
+		txtPassword.sendKeys(password);
+	}
+	
+	
+	public void hacerClickEnLogin() {	
+		buttonLogin.click();
+	}
+	
+}
---
+++ src/test/java/pruebas/MainTest.java
@@ -0,0 +1,67 @@
+package pruebas;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import paginas.PaginaInicio;
+
+public class MainTest {
+
+	String url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	WebDriver driver;
+	
+	@BeforeSuite //rutina para abrir el navegador
+	public void abrirNavegador() { //se suele llamar setUp()
+		// 1) Configurando el navegador Chrome
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(options);
+		// 2) Abrir el navegador en la página donde haremos la prueba
+		driver.get(url);
+//		3)  maximizar el navegador
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void completarCampos() {
+		PaginaInicio newSession = new PaginaInicio(driver);
+		
+		WebDriverWait myWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		myWait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+		
+		newSession.ingresarCredenciales("Admin","admin123");
+
+		
+	}
+	
+	@Test
+	public void iniciarSesion() {
+		PaginaInicio newSession = new PaginaInicio(driver);
+		
+//		Agrego demora para empezar a enviar la información a los campos
+
+		WebDriverWait myWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		myWait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+
+		newSession.ingresarCredenciales("Admin","admin123");
+
+		newSession.hacerClickEnLogin();
+	}
+	
+ @AfterSuite
+	public void cerrarNavegador() throws InterruptedException { //se suele llamar tearDown()
+     Thread.sleep(10*1000);//espero 10 segundos antes de cerrar la pagina	
+	 driver.close();
+		
+	}
+}
+
+
